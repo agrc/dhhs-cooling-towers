d570a9c5b897d7f6798907d1cf5d0a6c3c1e6582
Erik Neemann <eneemann@utah.gov>
2023-04-12 16:58:36 -0600

    refactor: CLI reorganization

--- cool_cli.py
+++ cool_cli.py
@@ -4,8 +4,9 @@
 DHHS Cooling Tower object detection
 
 Usage:
-    cool_cli.py download tiles <col> <row> [--save-to=location] (--mosaic --detect --parse-results)
-    cool_cli.py detect towers <file_name>
+    cool_cli.py download-tiles <col> <row> [--save-to=location --mosaic]
+    cool_cli.py detect-towers <file_name> [(--locate-results <col> <row>)] 
+    cool_cli.py process-tiles <col> <row> [--save-to=location]
 
 Options:
     --from=location                 The bucket or directory to operate on
@@ -13,10 +14,11 @@ Options:
     --instances=size                The number of containers running the job [default: 10]
     --save-to=location              The location to output the stuff
 Examples:
-    python cool_cli.py download tiles 198259 394029 --save-to=./data --mosaic
-    python cool_cli.py download tiles 198402 394067 --mosaic --detect
-    python cool_cli.py download tiles 198402 394067 --mosaic --detect --parse-results
-    python cool_cli.py detect towers ./data/198259_394029.jpg
+    python cool_cli.py download-tiles 198259 394029 --save-to=./tiles
+    python cool_cli.py download-tiles 198259 394029 --save-to=./mosaics --mosaic
+    python cool_cli.py detect-towers ./mosaics/198263_394029_mosaic.jpg
+    python cool_cli.py detect-towers ./mosaics/198263_394029_mosaic.jpg --locate-results 198263 394029
+    python cool_cli.py process-tiles 198263 394029 --save-to=./results
 
 """
 
@@ -40,34 +42,56 @@ def main():
     """doc string"""
     args = docopt(__doc__, version="1.0")  # type: ignore
 
-    if args["download"] and args["tiles"]:
+    if args["download-tiles"]:
         output_directory = None
 
-        print('downloading tiles ...')
+        print("downloading tiles ...")
+        if args["--save-to"]:
+            output_directory = Path(args["--save-to"])
+
         col, row = args["<col>"], args["<row>"]
-        tiles = cool.download_tiles(col, row)
+        tiles = cool.download_tiles(col, row, output_directory)
 
         if args["--mosaic"]:
-            if args["--save-to"]:
-                output_directory = Path(args["--save-to"])
-            
+            print("building mosaic ...")
+
             mosaic_image = cool.build_mosaic_image(tiles, col, row, output_directory)
-
-        if args["--detect"]:
-            results = cool.detect_towers(mosaic_image)
-
-            results.print()
-
-        if args["--parse-results"]:
-            results = cool.parse_results(results, col, row)
 
         return
 
-    if args["detect"] and args["towers"]:
+    if args["detect-towers"]:
         image = Path(args["<file_name>"])
         results = cool.detect_towers(image)
 
         results.print()
+
+        if args["--locate-results"]:
+            col, row = args["<col>"], args["<row>"]
+            results_df = cool.locate_results(results, col, row)
+
+            print(results_df.head().to_string())
+
+        return
+
+    if args["process-tiles"]:
+        output_directory = None
+
+        print("downloading tiles ...")
+        col, row = args["<col>"], args["<row>"]
+        tiles = cool.download_tiles(col, row, output_directory)
+
+        if args["--save-to"]:
+            output_directory = Path(args["--save-to"])
+
+        mosaic_image = cool.build_mosaic_image(tiles, col, row, output_directory)
+
+        results = cool.detect_towers(mosaic_image)
+
+        results.print()
+
+        results_df = cool.locate_results(results, col, row)
+
+        print(results_df.head().to_string())
 
         return
 
