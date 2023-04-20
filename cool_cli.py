05cddf0d37ae259e221b24307c27ce2a797b914f
Erik Neemann <eneemann@utah.gov>
2023-04-20 14:09:23 -0600

    cli: add full processing chain on specified rows

--- cool_cli.py
+++ cool_cli.py
@@ -7,6 +7,7 @@ Usage:
     cool_cli.py download-tiles <col> <row> [--save-to=location --mosaic]
     cool_cli.py detect-towers <file_name> [(--locate-results <col> <row>)] 
     cool_cli.py process-tiles <col> <row> [--save-to=location]
+    cool_cli.py process-upload <skip> <take>
 
 Options:
     --from=location                 The bucket or directory to operate on
@@ -19,6 +20,8 @@ Examples:
     python cool_cli.py detect-towers ./mosaics/198263_394029_mosaic.jpg
     python cool_cli.py detect-towers ./mosaics/198263_394029_mosaic.jpg --locate-results 198263 394029
     python cool_cli.py process-tiles 198263 394029 --save-to=./results
+    python cool_cli.py process-upload 10 20
+
 
 """
 
@@ -105,6 +108,40 @@ def main():
 
         return
 
+
+    if args["process-upload"]:
+        output_directory = None
+
+        rows = cool.get_rows_from_gbq(args["<skip>"], args["<take>"])
+
+        for row in rows:
+            print("downloading tiles ...")
+            tiles = cool.download_tiles(row.col_num, row.row_num, output_directory)
+
+            mosaic_image = cool.build_mosaic_image(tiles, row.col_num, row.row_num, output_directory)
+
+            results = cool.detect_towers(mosaic_image)
+
+            if not results:
+                print("no image available")
+
+                continue
+
+            results.print()
+
+            results_df = cool.locate_results(results, row.col_num, row.row_num)
+
+            if len(results_df.index) == 0:
+                print('no results to upload')
+
+                continue
+
+            cool.append_results(results_df)
+            print('appended results for col: {row.col_num} row: {row.row_num} ')
+
+            print(results_df.sort_values(by=['confidence'], ascending=True).head(20).to_string())
+        
+        return
 
 if __name__ == "__main__":
     main()
